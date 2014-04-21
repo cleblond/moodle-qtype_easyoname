5769618d02cf0bf816c9f8ecce63430755f90f1a
cleblond <carl.leblond@iup.edu>
2014-04-21 10:38:16 -0400

    updated copyright

--- question.php
+++ question.php
@@ -19,7 +19,7 @@
  *
  * @package    qtype
  * @subpackage easyoname
- * @copyright  2011 The Open University
+ * @copyright  2014 onwards Carl LeBlond
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
